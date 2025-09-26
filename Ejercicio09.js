9f0a97ce02c665bb1020a484f95e22c42b2662c2
Isaac Cabré Muñoz <isaaccabremunoz@gmail.com>
2025-09-26 13:05:34 +0200

    Ejercicio09 acabado

---
+++ Ejercicio09.js
@@ -0,0 +1,13 @@
+//Parte 1
+let letras = ["a", "b", "c", "d", "e"];
+
+letras.forEach(item =>   {
+    console.log(item)
+});
+
+//Parte 2
+let num = 5;
+
+for(var i = num; num >=0; num--){
+    console.log("Ahora el num vale: " + num)
+}
